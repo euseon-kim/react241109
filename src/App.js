bfcced64d46027743fd867a76d2f9d5f4a672de7
euseon-kim <yousun6101@naver.com>
2024-11-09 15:10:18 +0900

    241109

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Step01 from './step/step01';
+import Step02 from './step/step02';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* 컴포넌트 : 재사용 가능한 코드 조각(버튼, 이미지, 텍스트 ... ) */}
+      {/* <Comp />
+      <Comp />
+      <Comp />
+      <Comp />
+      <Comp />
+      <Step01></Step01> */}
+      <Step02 />
+    </>
+    
   );
 }
 
+// 작명 시에 영어대문자로 시작
+// 함수 표현식, 함수 선언식 둘 다 가능
+
+function Comp() {
+  return <div>컴포넌트</div>
+}
 export default App;
+
